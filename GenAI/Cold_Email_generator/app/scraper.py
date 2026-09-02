4dc7bafde5a1861257816b890823f77b08e2fbda
Prabhu-Jethi <prabhujethi9@gmail.com>
2026-09-02 22:50:31 +0530

    Initial commit

---
+++ GenAI/Cold_Email_generator/app/scraper.py
@@ -0,0 +1,23 @@
+import logging
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+from langchain_community.document_loaders import WebBaseLoader
+
+class Scrape():
+    def scrape_page_content(url):
+        try:
+            loader = WebBaseLoader(url)
+            page_data = loader.load()
+            return page_data[0].page_content if page_data else None
+        
+        except Exception as e:
+            logging.error(f"Failed to fetch {url}: {e}")
+            return None
+    
+
+if __name__ == "__main__":
+    page_url = "https://careers.nike.com/software-engineer-iii-itc/job/R-49969"
+    page_loader = Scrape()
+    page_data = page_loader.scrape_page_content()
+    print(page_data)
